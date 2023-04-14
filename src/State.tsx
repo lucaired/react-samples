facd8535cb72a653fa69456b3ce58ef0399ca57c
lucaired <70109317+lucaired@users.noreply.github.com>
2023-04-14 12:46:00 +0200

    Update state componet

--- src/State.tsx
+++ src/State.tsx
@@ -6,10 +6,21 @@ const State = () => {
         incrementCounter(counter + 1);
     };
 
+    const [name, setName] = React.useState<String>('John');
+
+    const handleInput = (name: String) => {
+        setName(name);
+    }
+
+    console.log('State component re-rendered');
+
+    // Will only re-render when counter changes
     return (
         <div>
             <p>Counter: {counter}</p>
             <button onClick={handleCounterClick}>Increment</button>
+            <p>Name: {name}</p>
+            <input type="text" onChange={(e) => handleInput(e.target.value)} />
         </div>
     );
 }
